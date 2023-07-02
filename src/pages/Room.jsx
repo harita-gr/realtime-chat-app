174db0dca4feb3708680de305ac3d8c7372e78cb
Harita Ravindranath <haritagr19@gmail.com>
2023-07-02 12:39:41 +0530

    create and read messages

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,7 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  databases,
+  DATABASE_ID,
+  COLLECTION_ID_MESSAGES,
+} from "../appwriteConfig";
+import { ID } from "appwrite";
 
 const Room = () => {
-  return <div>Room</div>;
+  const [messages, setMessages] = useState([]);
+  const [messageBody, setMessageBody] = useState("");
+
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let payload = {
+      body: messageBody,
+    };
+
+    let response = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES,
+      ID.unique(),
+      payload
+    );
+
+    console.log("Created!", response);
+    setMessages((prevState) => [response, ...prevState]);
+
+    setMessageBody("");
+  };
+
+  const getMessages = async () => {
+    const response = await databases.listDocuments(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES
+    );
+    setMessages(response.documents);
+  };
+
+  return (
+    <main className="container">
+      <div className="room--container">
+        <form id="message-form" onSubmit={handleSubmit}>
+          <div>
+            <textarea
+              required
+              maxLength={1000}
+              placeholder="Say something..."
+              value={messageBody}
+              onChange={(e) => {
+                setMessageBody(e.target.value);
+              }}
+            />
+          </div>
+          <div className="send-btn--wrapper">
+            <input className="btn btn--secondary" type="submit" value="Send" />
+          </div>
+        </form>
+        <div>
+          {messages?.map((message) => {
+            return (
+              <div key={message.$id} className="message--wrapper">
+                <div className="message--header">
+                  <small className="message-timestamp">
+                    {message.$createdAt}
+                  </small>
+                </div>
+                <div className="message--body">
+                  <span>{message.body}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Room;
