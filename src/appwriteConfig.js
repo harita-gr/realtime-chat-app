e9a3b005f4129390904f8a17473320303c52921c
Harita Ravindranath <haritagr19@gmail.com>
2023-07-02 12:39:29 +0530

    appwrite client & db setup

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,9 +1,15 @@
-import { Client } from "appwrite";
+import { Client, Databases } from "appwrite";
+
+export const PROJECT_ID = "649dfaa5ba598a38b068";
+export const DATABASE_ID = "649dfdc56211136c2aa0";
+export const COLLECTION_ID_MESSAGES = "649dfdd56674329f61ab";
 
 const client = new Client();
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject("649dfaa5ba598a38b068");
+
+export const databases = new Databases(client);
 
 export default client;
